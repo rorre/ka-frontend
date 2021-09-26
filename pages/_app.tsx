ced6a312c2debcc4e205c2cfe0578da1f0137519
codewithdjaeger <spiritjaeger@gmail.com>
2021-09-26 13:09:53 +0700

    🛠 Bootstrapping

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
+import React from 'react'
+import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+/**
+ * 描述
+ * @date 2021-09-26
+ * @param {any} {Component
+ * @param {any} pageProps}:AppProps
+ * @return {any}
+ */
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
 export default MyApp
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+	name: string
 }
 
+/**
+ * Give John Doe Response
+ * @date 2021-09-26
+ * @param {NextApiRequest} req:NextApiRequest
+ * @param {NextApiResponse<string>} res:NextApiResponse<Data>
+ */
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+	res.status(200).json({ name: 'John Doe' })
 }
