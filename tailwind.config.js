6cc4a5bd64471fa6fde168b0ed792155ce76bdf7
nayyara-airlangga <nayyara.airlangga@gmail.com>
2021-10-24 00:35:37 +0700

    ⚒ BREAKING CHANGE

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
 	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		container: {
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1080px',
+			},
+		},
+		extend: {
+			fontFamily: { sans: ['Poppins'] },
+			colors: {
+				'dark-1': '#13111A',
+				'dark-2': '#302C3F',
+				'dark-3': '#5E5B71',
+				'blue-dark': '#164F85',
+			},
+		},
 	},
 	variants: {
 		extend: {},
