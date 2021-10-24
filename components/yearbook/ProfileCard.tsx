b27e26e08c24fc1f0be0d7a07420dca14a3c735f
nayyara-airlangga <nayyara.airlangga@gmail.com>
2021-10-25 03:50:57 +0700

    🔨 Finished profile card 🔧 Fix search button (previously unclickable)

--- components/yearbook/ProfileCard.tsx
+++ components/yearbook/ProfileCard.tsx
@@ -6,12 +6,13 @@ import * as utils from '../../utils/utils'
 
 const ProfileCard = ({ name, major, image, house }: Student) => {
 	return (
-		<StyledProfileCard className='bg-dark-4 py-2 pl-2 m-1.5 border-4 relative rounded-xl border-dark-2'>
+		<div className='bg-dark-4 py-2 pl-2 m-1.5 border-4 relative rounded-xl border-dark-2'>
+			<UpperNotch />
 			<article>
-				<div className='flex bg-dark-4 border-dark-2'>
+				<div className='flex bg-dark-4 divide-x-2 divide-dashed divide-dark-2 border-dark-2'>
 					<ProfilePictureContainer image={image} />
-					<div className='flex-col w-7/12 ml-3 bg-dark-4 relative'>
-						<div className='order-1 text-white text-sm md:ml-0 ml-2 mt-5 bg-dark-4 mr-1 mb-1'>
+					<div className='flex-col w-7/12 ml-4 pl-3 bg-dark-4 relative'>
+						<div className='order-1 text-white text-sm md:ml-0 md:mt-6 ml-2 mt-5 bg-dark-4 mr-1 mb-1'>
 							{utils.shortenName(name)}
 						</div>
 						<div className='order-2 text-white md:ml-0 ml-2 font-extralight text-xs bg-dark-4 mr-1'>
@@ -20,23 +21,63 @@ const ProfileCard = ({ name, major, image, house }: Student) => {
 						<div className='order-3 flex justify-start mt-1 bg-dark-4 md:mr-3 mr-1'>
 							{utils.getHouseIcon(house)}
 						</div>
-						<div className='order-4 flex justify-end bg-dark-4 md:mt-3 mt-9 mr-1'>
-							<Social.LineIcon width='md:w-7 w-12' height='md:h-7 h-11' />
-							<Social.InstagramIcon width='md:h-7 w-12' height='md:h-7 h-11' />
+						<div className='order-4 flex justify-end bg-dark-4 md:mt-0 mt-9 mr-1'>
+							<Social.LineIcon width='md:w-7 w-10' height='md:h-7 h-10' />
+							<Social.InstagramIcon width='md:h-7 w-10' height='md:h-7 h-10' />
 						</div>
 					</div>
 				</div>
 			</article>
-		</StyledProfileCard>
+			<LowerNotch />
+		</div>
 	)
 }
 
-const StyledProfileCard = styled.div``
+const UpperNotch = styled.div`
+	position: absolute;
+	top: -4px;
+	right: 43%;
+	height: 2rem;
+	width: 2.65rem;
+	z-index: 2;
+	background-color: #13111a;
+	border-color: #302c3f;
+	border-width: 0 4px 4px 4px;
+	border-bottom-right-radius: 50%;
+	border-bottom-left-radius: 50%;
+	display: inline-block;
+
+	@media screen and (max-width: 767px) {
+		height: 2rem;
+		width: 2.8rem;
+		right: 45.5%;
+	}
+`
+const LowerNotch = styled.div`
+	position: absolute;
+	bottom: -4px;
+	right: 43%;
+	height: 2rem;
+	width: 2.65rem;
+	z-index: 0;
+	background-color: #13111a;
+	border-color: #302c3f;
+	border-width: 4px 4px 0 4px;
+	border-top-right-radius: 50%;
+	border-top-left-radius: 50%;
+	display: inline-block;
+
+	@media screen and (max-width: 767px) {
+		height: 2rem;
+		width: 2.8rem;
+		right: 45.5%;
+	}
+`
 
 export default ProfileCard
 
 const ProfilePictureContainer = ({ image }: { image: string }) => (
-	<div className={`rounded-xl w-5/12 border-4 rounded-xl border-dark-2`}>
+	<div className='rounded-xl w-5/12 border-4 rounded-xl border-dark-2 mr-1.5'>
 		<ProfilePicture className='h-full' src={image} alt='Profile-Image' />
 	</div>
 )
