528f74f218c55c91dcc1d1ea61da2f5bcd541a7b
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-11-04 10:23:28 +0700

    🛠 created Logo

---
+++ app/components/global/navbar/styles/Logo.styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Logo = styled.span`
+	background: -webkit-linear-gradient(150deg, #ff5656 30.1%, #2a98ff 70.01%);
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+`
+
+export default Logo
