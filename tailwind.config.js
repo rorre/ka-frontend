d0eb40a1c06af88dd72c4e85c223e2ac455ebc6a
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-11-04 10:15:13 +0700

    🔧 removed duplicate and extends dropshadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,29 +23,27 @@ module.exports = {
 
 			'2xl': '1536px',
 			// => @media (min-width: 1536px) { ... }
-
-			container: {
-				screens: {
-					sm: '640px',
-					md: '768px',
-					lg: '1024px',
-					xl: '1080px',
-				},
+		},
+		extend: {
+			fontFamily: { sans: ['Poppins'] },
+			colors: {
+				'dark-1': '#13111A',
+				'dark-2': '#302C3F',
+				'dark-3': '#5E5B71',
+				'dark-4': '#1D1A27',
+				'blue-dark': '#164F85',
+				'red-dark': '#AB1818',
 			},
-			extend: {
-				fontFamily: { sans: ['Poppins'] },
-				colors: {
-					'dark-1': '#13111A',
-					'dark-2': '#302C3F',
-					'dark-3': '#5E5B71',
-					'dark-4': '#1D1A27',
-					'blue-dark': '#164F85',
-				},
+			dropShadow: {
+				'blue-dark': '0 25px 25px rgba(255, 255, 255, 1)',
 			},
 		},
-		variants: {
-			extend: {},
-		},
-		plugins: [],
 	},
+	variants: {
+		extend: {
+			dropShadow: ['hover'],
+			backgroundImage: ['group-hover', 'hover'],
+		},
+	},
+	plugins: [],
 }
