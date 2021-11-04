fff975c959dcd7a4b473a7ef4c945b938ede7662
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-11-04 20:30:57 +0700

    🔧 unwrapped NavLink

--- app/components/global/navbar/NavLink.tsx
+++ app/components/global/navbar/NavLink.tsx
@@ -4,14 +4,12 @@ import { NavLinkInterface } from './interfaces/NavLink.interface'
 
 const NavLink = ({ label, url, className = null }: NavLinkInterface) => {
 	return (
-		<div className='flex justify-center col-span-1'>
-			<Link href={url} passHref>
+		<Link href={url} passHref>
 			<a
 				className={`hover:text-blue-dark filter hover:drop-shadow-blue-dark ${className}`}>
-					{label}
-				</a>
-			</Link>
-		</div>
+				{label}
+			</a>
+		</Link>
 	)
 }
 
