3d9455c9e0cb2d8acf07cf9f2faec3264a13f930
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-11-04 10:16:48 +0700

    🛠 created  LoginButton

---
+++ app/components/global/navbar/LoginButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginButton = () => {
+	return (
+		<div className='p-1 rounded-full from-blue-dark to-red-dark bg-gradient-to-r group hover:bg-none'>
+			<button className='py-3 font-semibold text-white rounded-full md:col-span-1 bg-dark-2 px-9 group-hover:bg-gradient-to-r from-blue-dark to-red-dark'>
+				Login SSO
+			</button>
+		</div>
+	)
+}
+
+export default LoginButton
