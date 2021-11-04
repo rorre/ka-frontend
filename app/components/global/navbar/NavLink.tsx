986f73372bec6786bed2e2dfb91c819ef3007a9f
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-11-04 20:29:47 +0700

    🔨 added classname props to NavLink

--- app/components/global/navbar/NavLink.tsx
+++ app/components/global/navbar/NavLink.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Link from 'next/link'
 import { NavLinkInterface } from './interfaces/NavLink.interface'
 
-const NavLink = ({ label, url }: NavLinkInterface) => {
+const NavLink = ({ label, url, className = null }: NavLinkInterface) => {
 	return (
 		<div className='flex justify-center col-span-1'>
 			<Link href={url} passHref>
-				<a className='hover:text-blue-dark filter hover:drop-shadow-blue-dark'>
+			<a
+				className={`hover:text-blue-dark filter hover:drop-shadow-blue-dark ${className}`}>
 					{label}
 				</a>
 			</Link>
