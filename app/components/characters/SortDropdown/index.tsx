3839b668380c55c6c937579a1094815956b99982
nayyara-airlangga <nayyara.airlangga@gmail.com>
2021-11-14 20:50:27 +0700

    🔧 Fix form handling on yb page

--- app/components/characters/SortDropdown/index.tsx
+++ app/components/characters/SortDropdown/index.tsx
@@ -5,8 +5,8 @@ import { StyledRadio } from './styles'
 const SortDropdown = () => {
 	const [isDropdown, setIsDropdown] = useState(false)
 	const [sortBy, setSortBy] = useState('')
-	const [isNameChecked, setIsNameChecked] = useState(false)
-	const [isNPMChecked, setIsNPMChecked] = useState(false)
+	const [isAscending, setIsAscending] = useState(false)
+	const [isDescending, setIsDescending] = useState(false)
 
 	return (
 		<div className='md:h-full h-6 relative inline-block bg-dark-2 md:px-5 px-3 lg:py-2.5 md:py-1.5 lg:rounded-xl md:rounded-md rounded-md border-black lg:w-3/12 md:w-4/12 w-5/12 text-gray-400'>
@@ -15,7 +15,7 @@ const SortDropdown = () => {
 				type='button'
 				className='bg-dark-2 inline-flex w-full text-gray-400'>
 				<span className='w-11/12 text-left md:text-sm lg:text-base text-2xs bg-dark-2'>
-					{isNameChecked ? 'Name' : isNPMChecked ? 'NPM' : 'Sort by'}
+					{isAscending ? 'Ascending' : isDescending ? 'Descending' : 'Sort by'}
 				</span>
 				<svg
 					className={`w-1/12 absolute right-3.5 lg:top-2.5 top-1.5 md:mt-1 bg-dark-2 ${
@@ -37,49 +37,51 @@ const SortDropdown = () => {
 				{isDropdown && (
 					<div className='z-10 absolute right-0 w-full mt-4 text-gray-400 bg-dark-2 rounded-xl'>
 						<button
+							type='button'
 							onClick={() => {
-								setSortBy('Name')
-								setIsNameChecked(true)
-								setIsNPMChecked(false)
+								setSortBy('asc')
+								setIsAscending(true)
+								setIsDescending(false)
 							}}
 							className='relative text-left bg-dark-3 hover:bg-dark-2 md:px-5 py-2 px-3 md:text-sm lg:text-base text-2xs w-full rounded-t-lg border-dark-2'>
-							Name
-							<label htmlFor='name'></label>
+							Ascending
+							<label htmlFor='asc'></label>
 							<StyledRadio
 								type='radio'
-								id='name'
-								name='sortBy'
+								id='asc'
+								name='sort'
 								className='absolute md:right-4 right-2 md:top-3.5 top-2.5'
 								value={sortBy}
 								onChange={() => {
-									setSortBy('Name')
-									setIsNameChecked(true)
-									setIsNPMChecked(false)
+									setSortBy('asc')
+									setIsAscending(true)
+									setIsDescending(false)
 								}}
-								checked={isNameChecked}
+								checked={isAscending}
 							/>
 						</button>
 						<button
+							type='button'
 							onClick={() => {
-								setSortBy('Name')
-								setIsNPMChecked(true)
-								setIsNameChecked(false)
+								setSortBy('desc')
+								setIsDescending(true)
+								setIsAscending(false)
 							}}
 							className='relative text-left bg-dark-3 hover:bg-dark-2 text-gray-400 w-full md:px-5 py-2 px-3 md:text-sm lg:text-base text-2xs rounded-b-lg border-dark-2'>
-							NPM
-							<label htmlFor='npm'></label>
+							Descending
+							<label htmlFor='desc'></label>
 							<StyledRadio
 								type='radio'
-								id='npm'
-								name='sortBy'
+								id='desc'
+								name='sort'
 								value={sortBy}
 								className='absolute md:right-4 right-2 md:top-3.5 top-2.5'
 								onChange={() => {
-									setSortBy('NPM')
-									setIsNPMChecked(true)
-									setIsNameChecked(false)
+									setSortBy('desc')
+									setIsDescending(true)
+									setIsAscending(false)
 								}}
-								checked={isNPMChecked}
+								checked={isDescending}
 							/>
 						</button>
 					</div>
