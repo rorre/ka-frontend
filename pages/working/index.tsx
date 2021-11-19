02b5082deb54d504ca90ca2aa7ba29cbf701e801
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-11-20 05:43:34 +0700

    🔧fixed sizing

--- pages/working/index.tsx
+++ pages/working/index.tsx
@@ -3,28 +3,24 @@ import Image from 'next/image'
 
 const index = () => {
 	return (
-		<div className='w-full h-screen'>
-			<section className='flex items-center justify-center w-full h-screen'>
-				<div className='flex flex-col items-center justify-center gap-6'>
-					<div className='text-center text-white'>
-						<h1 className='text-4xl font-bold'>
-							The page is under development
-						</h1>
-						<h2 className='text-lg font-medium'>
-							Sorry for the inconvenience, but we’re working on it!
-						</h2>
-					</div>
-					<div>
-						<Image
-							src='/assets/under_development.svg'
-							alt='Under Development'
-							width={727}
-							height={352}
-						/>
-					</div>
+		<section className='flex items-center justify-center w-full h-screen bg-no-repeat bg-contain bg-shade-pattern-sm lg:bg-shade-pattern lg:bg-cover'>
+			<div className='flex flex-col items-center justify-center gap-6'>
+				<div className='text-center text-white'>
+					<h1 className='text-4xl font-bold'>The page is under development</h1>
+					<h2 className='text-lg font-medium'>
+						Sorry for the inconvenience, but we’re working on it!
+					</h2>
 				</div>
-			</section>
-		</div>
+				<div>
+					<Image
+						src='/assets/under_development.svg'
+						alt='Under Development'
+						width={727}
+						height={352}
+					/>
+				</div>
+			</div>
+		</section>
 	)
 }
 
