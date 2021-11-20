c620ca63f3615badba4988bf79b223b2ba3fa4b1
Ren <renrror@gmail.com>
2021-11-20 15:15:18 +0700

    Revert "Yearbook Improvements"

--- app/components/characters/SearchBar/SearchBarButton.tsx
+++ app/components/characters/SearchBar/SearchBarButton.tsx
@@ -4,7 +4,7 @@ import { SearchButton } from './styles'
 export const SearchBarButton = () => (
 	<SearchButton
 		type='submit'
-		className={`text-white hover:opacity-100 active:opacity-100 opacity-80 lg:w-2/12 md:w-2/12 w-3/12 inline-block md:py-3.5 py-2 text-center md:rounded-lg rounded md:text-base text-1.5xs`}>
+		className={`text-white hover:opacity-100 active:opacity-100 opacity-80 lg:w-2/12 md:w-2/12 w-3/12 inline-block md:py-3.5 py-2 text-center md:rounded-lg rounded md:text-base text-2xs`}>
 		Search
 	</SearchButton>
 )
--- app/components/characters/SearchBar/interfaces/SearchBarDropdown.interface.tsx
+++
@@ -1,3 +0,0 @@
-export interface SearchBarDropdownInterface {
-	className?: string
-}
