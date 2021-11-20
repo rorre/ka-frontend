d6c49ca494968ff0a3661132faf6a1ab5fde4281
Rendy Arya Kemal <renrror@gmail.com>
2021-11-20 11:35:57 +0700

    🔧 Fix missing fonts

--- app/components/global/header/index.tsx
+++ app/components/global/header/index.tsx
@@ -4,6 +4,9 @@ import Head from 'next/head'
 const Header = () => {
 	return (
 		<Head>
+			<link
+				href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Poppins:wght@600&family=Sora:wght@400;600;700&display=swap'
+				rel='stylesheet'></link>
 			<title>CSUI 2021</title>
 			<meta name='description' content='Karya Angkatan CSUI 2021' />
 			<link rel='icon' href='/favicon.ico' />
